b16ce4c4d0af176fdfce4e877d6971fc5e829c12
Malcolm Kapuza <malcolm.kapuza@gmail.com>
2020-04-13 11:51:06 -0700

    Add privacy policy

---
+++ src/pages/privacy.js
@@ -0,0 +1,133 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const PrivacyPage = () => (
+  <Layout>
+    <SEO title="Privacy Policy" />
+    <h1>Privacy Policy</h1>
+    <p>
+      This policy applies to all information collected or submitted on Commit’s
+      website and our apps for iPhone and any other devices and platforms.
+    </p>
+    <h2>Information we collect</h2>
+
+    <p>
+      Email addresses are only used for logging in, password resets, responding
+      to emails that you initiate, and sending notifications that you request.
+      We don’t send promotional emails.
+    </p>
+    <p>
+      We store information about your Health data to sync this information
+      between the website and your devices.
+    </p>
+    <h2>Technical basics</h2>
+
+    <p>
+      If you enable notifications, we must store a token to send them. We never
+      use notifications for marketing.
+    </p>
+
+    <p>
+      If you upload files to Commit, we need to store them until you delete
+      them.
+    </p>
+    <p>
+      We use cookies on the site and similar tokens in the app to keep you
+      logged in.
+    </p>
+    <p>
+      Our server software may also store basic technical information, such as
+      your IP address, in temporary memory or logs.
+    </p>
+
+    <h2>Information usage</h2>
+
+    <p>
+      We use the information we collect to operate and improve our website,
+      apps, and customer support.
+    </p>
+
+    <p>We do not share personal information with outside parties</p>
+
+    <p>
+      We may disclose your information in response to subpoenas, court orders,
+      or other legal requirements; to exercise our legal rights or defend
+      against legal claims; to investigate, prevent, or take action regarding
+      illegal activities, suspected fraud or abuse, violations of our policies;
+      or to protect our rights and property.
+    </p>
+
+    <p>
+      In the future, we may sell to, buy, merge with, or partner with other
+      businesses. In such transactions, user information may be among the
+      transferred assets.
+    </p>
+    <h2>Security</h2>
+
+    <p>
+      We implement a variety of security measures to help keep your information
+      secure. For instance, all communication with the app and website requires
+      HTTPS with certificate pinning. Passwords are hashed, not stored, using
+      industry-standard methods.
+    </p>
+
+    <h2>Accessing, changing, or deleting information</h2>
+
+    <p>
+      Email <a href="mailto:info@commitapp.info">info@commitapp.info</a> to
+      request your account to be deleted
+    </p>
+
+    <p>
+      Deleted information may be kept in backups for up to 90 days. Backups are
+      encrypted and are only accessed if needed for disaster recovery.
+    </p>
+
+    <p>
+      Commit may delete your information at any time and for any reason, such as
+      technical needs, legal concerns, abuse prevention, removal of idle
+      accounts, data loss, or any other reason.
+    </p>
+    <h2>California Online Privacy Protection Act Compliance</h2>
+
+    <p>
+      We comply with the California Online Privacy Protection Act. We therefore
+      will not distribute your personal information to outside parties without
+      your consent.
+    </p>
+    <h2>Children’s Online Privacy Protection Act Compliance</h2>
+
+    <p>
+      We never collect or maintain information at our website from those we
+      actually know are under 13, and no part of our website is structured to
+      attract anyone under 13.
+    </p>
+    <h2>Information for European Union Customers</h2>
+
+    <p>
+      By using Commit and providing your information, you authorize us to
+      collect, use, and store your information outside of the European Union.
+    </p>
+    <h2>International Transfers of Information</h2>
+
+    <p>
+      Information may be processed, stored, and used outside of the country in
+      which you are located. Data privacy laws vary across jurisdictions, and
+      different laws may be applicable to your data depending on where it is
+      processed, stored, or used.
+    </p>
+    <h2>Your Consent</h2>
+
+    <p>By using our site or apps, you consent to our privacy policy.</p>
+    <h2>Contacting Us</h2>
+
+    <p>
+      If you have questions regarding this privacy policy, you may email{" "}
+      <a href="mailto:info@commitapp.info">info@commitapp.info</a>.
+    </p>
+  </Layout>
+)
+
+export default PrivacyPage
